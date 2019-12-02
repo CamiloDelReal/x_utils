38810ccf3933c5fba272ada4b260edbe9ff5d5f5
Camilo José del Real Martell <camilo.martell@copsonic-tech.com>
2019-12-02 22:05:54 +0000

    Create "X Utils"

---
+++ src/pathutils.cpp
@@ -0,0 +1,30 @@
+#include <QFileInfo>
+#include <QDir>
+#include <QStandardPaths>
+
+#include "pathutils.hpp"
+
+void PathUtils::makeParentPath(const QString &filePath)
+{
+    QFileInfo info(filePath);
+    QDir dir(info.absoluteDir());
+    if(!dir.exists())
+    {
+        dir.mkpath(info.absoluteDir().absolutePath());
+    }
+}
+
+QString PathUtils::appicationDataPath()
+{
+    QString path;
+#if defined (Q_OS_ANDROID)
+    path = QStandardPaths::standardLocations(QStandardPaths::AppDataLocation).value(1);
+#elif defined (Q_OS_IOS)
+    path = QStandardPaths::standardLocations(QStandardPaths::AppDataLocation).value(0);
+#elif defined (Q_OS_WIN)
+    path = QStandardPaths::standardLocations(QStandardPaths::AppDataLocation).value(0);
+#elif defined (Q_OS_LINUX)
+    path = QStandardPaths::standardLocations(QStandardPaths::AppDataLocation).value(0);
+#endif
+    return path;
+}
---
+++ src/pathutils.hpp
@@ -0,0 +1,26 @@
+#ifndef PATHUTILS_HPP
+#define PATHUTILS_HPP
+
+#include <QObject>
+#include <QRegularExpression>
+
+namespace PathUtils
+{
+
+#if defined (Q_OS_ANDROID)
+static const QString SEPARATOR = QStringLiteral("/");
+#elif defined (Q_OS_IOS)
+static const QString SEPARATOR = QStringLiteral("/");
+#elif defined (Q_OS_WIN)
+static const QString SEPARATOR = QStringLiteral("\\");
+#elif defined (Q_OS_LINUX)
+static const QString SEPARATOR = QStringLiteral("/");
+#endif
+
+void makeParentPath(const QString &filePath);
+
+QString appicationDataPath();
+
+}
+
+#endif //PATHUTILS_HPP
